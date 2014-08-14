33b073149559650e6425dbadd1ae073b98d8dc33
Matt Chan & Jared <pair+matt16749.brasschaos@devbootcamp.com>
2014-08-14 11:29:30 -0700

    created app/view and app/controller - index.rb and index.erb - fixed migrations

--- db/migrate/20140814110910_create_questions.rb
+++ db/migrate/20140814110910_create_questions.rb
@@ -1,9 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
-    create_table questions do |t|
+    create_table :questions do |t|
       t.integer :survey_id
       t.string :content
       t.boolean :answer
       t.timestamps
+    end
   end
 end
--- db/migrate/20140814110926_create_surveys.rb
+++ db/migrate/20140814110926_create_surveys.rb
@@ -1,6 +1,6 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
-    create_table surveys do |t|
+    create_table :surveys do |t|
       t.string :title
       t.string :author
       t.timestamps
